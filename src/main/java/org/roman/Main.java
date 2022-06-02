0ac319ac7ce6af6285fdc114757264f6189119c5
error <romank.official@gmail.com>
2022-06-02 12:53:34 +0300

    added spring annotation config and java config

--- src/main/java/org/roman/Main.java
+++ src/main/java/org/roman/Main.java
@@ -10,7 +10,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Spring XML configuration");
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("bean-factory.xml");
+        BeanFactory beanFactory =
+                new ClassPathXmlApplicationContext("bean-factory.xml");
         Calculator calculator = beanFactory.getBean("calculator", org.roman.xmlconfig.Calculator.class);
         System.out.println("Summa : " + calculator.calculate(3, 5));
         System.out.println("Diferent : " + calculator.calculate(6, 5));
